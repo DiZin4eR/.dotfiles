3c489431ac335b98cb4cb3789d02c7f562c83b23
Dmytro Zinchuk <dizin4er@gmail.com>
2025-07-01 16:52:28 +0200

    Refactoring

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,15 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 bind \cl 'clear; fastfetch; commandline -f repaint'
 
+# Start ssh-agent if not running
+if not pgrep -u (id -u) ssh-agent > /dev/null
+    eval (ssh-agent -c)
+end
+
+# Add key if not already added
+ssh-add -l | grep "ED25519" > /dev/null; or ssh-add ~/.ssh/id_ed25519 > /dev/null
+
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     
